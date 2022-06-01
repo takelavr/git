61166d1f77de0d2c88a44cb4cda4c6777f5012a8
Desktop <96823498+takelavr@users.noreply.github.com>
2022-06-01 18:06:20 +0400

    Desktop

--- lessons/src/Solution.java
+++ lessons/src/Solution.java
@@ -9,14 +9,12 @@ package com.javarush.task.task33.task3308;
 import org.junit.platform.commons.util.StringUtils;
 
 public class Solution {
-        public static String mouthSize(String animal){
-            if (animal.equalsIgnoreCase("alligator")) return "small";
-            return "wide";
-        }
+    public static String mouthSize(String animal) {
+        if (animal.equalsIgnoreCase("alligator")) return "small";
+        return "wide";
 
 
-
-
+    }
 
 
     public static void main(String[] args) {
